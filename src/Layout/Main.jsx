d8df2fa065bb62f8cae63e80fdd1bafbbf612dd0
Li Pon <mirnatul@gmail.com>
2023-10-06 04:16:57 +0600

    dashboard layout completed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,72 +3,87 @@ import { Link, NavLink, Outlet } from 'react-router-dom';
 import mainLogo from '../../src/assets/logo.png'
 import { PiDotsThreeOutlineVerticalBold } from 'react-icons/pi'
 import { CgClose } from 'react-icons/cg'
+import { AiOutlineHome } from 'react-icons/ai'
+import { RiDashboardLine } from 'react-icons/ri'
+import { FaRegUser } from 'react-icons/fa'
+import { FiSettings } from 'react-icons/fi'
+import { GoSignOut, GoSignIn } from 'react-icons/go'
+import './Main.css'
+
 
 const Main = () => {
     // const { user } = useContext(AuthContext)
+    const user = false
+    const [enabled, setEnabled] = useState(false)
     const [isOpen, setIsOpen] = useState(false);
     const navItem = (
         <>
             <li>
                 <NavLink
                     to="/"
+                    // className={`${({ isActive }) => (isActive ? "d-active bg-yellow-400 hover:bg-yellow-600 font-bold font-slate-700" : "d-default text-white")} flex items-center text-xl mt-2 py-2 px-2`}
                     className={({ isActive }) => (isActive ? "d-active" : "d-default")}
                 >
-                    <span className="mr-6">
-                        {/* <AiFillHome size={24}></AiFillHome> */}
+                    <span className="mr-3">
+                        <AiOutlineHome></AiOutlineHome>
                     </span>
                     <span>Home</span>
                 </NavLink>
             </li>
             <li>
                 <NavLink
-                    to="/userDashboard/profile"
+                    to="/dashboard"
                     className={({ isActive }) => (isActive ? "d-active" : "d-default")}
                 >
-                    <span className="mr-6">
-                        {/* <FaUserAlt size={24}></FaUserAlt> */}
+                    <span className="mr-3">
+                        <RiDashboardLine></RiDashboardLine>
+                    </span>
+                    <span>Dashboard</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/profile"
+                    className={({ isActive }) => (isActive ? "d-active" : "d-default")}
+                >
+                    <span className="mr-3">
+                        <FaRegUser></FaRegUser>
                     </span>
                     <span>Profile</span>
                 </NavLink>
             </li>
             <li>
                 <NavLink
-                    to="/userDashboard/ticketHistory"
+                    to="/help"
                     className={({ isActive }) => (isActive ? "d-active" : "d-default")}
                 >
-                    <span className="mr-6">
-                        {/* <FaHistory size={24}></FaHistory> */}
+                    <span className="mr-3">
+                        <FiSettings></FiSettings>
                     </span>
-                    <span>Ticket History</span>
+                    <span>Help</span>
                 </NavLink>
             </li>
-            <li>
-                <NavLink
-                    to="/userDashboard/favorite"
-                    className={({ isActive }) => (isActive ? "d-active" : "d-default")}
-                >
-                    <span className="mr-6">
-                        {/* <MdOutlineFavorite size={24}></MdOutlineFavorite> */}
-                    </span>
-                    <span>Favorite Movies</span>
-                </NavLink>
+            <li className='lg:mt-[100%] d-default'>
+                <span>Mode</span>
             </li>
-            <li>
-                <NavLink
-                    to="/"
-                    className={({ isActive }) => (isActive ? "d-active" : "d-default")}
-                >
-                    <span className="mr-6">
-                        {/* <FaSignOutAlt size={24}></FaSignOutAlt> */}
+            {user ? <li className='text-xl d-default'>
+                <span className="mr-3">
+                    <GoSignOut></GoSignOut>
+                </span>
+                <span>Logout</span>
+            </li> :
+                <li className='text-xl d-default'>
+                    <span className="mr-3">
+                        <GoSignIn></GoSignIn>
                     </span>
-                    <span>Sign Out</span>
-                </NavLink>
-            </li>
+                    <span>Sign In</span>
+                </li>
+            }
         </>
     );
     return (
         <div>
-            <div className="flex lg:flex-row flex-col text-white bg-slate-900 min-h-screen overflow-hidden max-w-[1420px] mx-auto">
+            <div className="flex lg:flex-row flex-col bg-slate-900 min-h-screen overflow-hidden max-w-[1420px] mx-auto">
                 <div className="lg:border-r-4 p-2 lg:w-[350px]">
                     {/* Site Logo */}
                     <img className='w-[250px] mx-auto mt-4 hidden lg:block' src={mainLogo} alt="" />
@@ -81,20 +96,20 @@ const Main = () => {
                         <div><PiDotsThreeOutlineVerticalBold size={24}></PiDotsThreeOutlineVerticalBold></div>
                     </div>
                     <hr className='border-1 mt-4 w-11/12 mx-auto lg:hidden' />
-                    {/* </div> */}
-                    {/* Nav Items */}
-                    <div className="hidden lg:block">
+
+                    {/* nav items for pc */}
+                    <div className="hidden lg:block mt-12">
                         <ul>{navItem}</ul>
                     </div>
-                    {/* Mobile Nav Item */}
+                    {/* nav items for mobile */}
                     <div className={`lg:hidden glass-bg w-[95%] transition-all duration-300 p-4 ${isOpen ? "fixed bottom-2 block z-50" : "hidden"}`}>
-                        <div className='flex justify-end'>
-                            <button onClick={() => setIsOpen(!isOpen)}><CgClose size={32}></CgClose></button>
+                        <div className='flex justify-end mb-1'>
+                            <button onClick={() => setIsOpen(!isOpen)}><CgClose size={32} color='rgb(250,204,21)'></CgClose></button>
                         </div>
-                        <ul className=" px-2">{navItem}</ul>
+                        <ul className="px-2">{navItem}</ul>
                     </div>
                 </div>
-                <div className="w-full lg:p-10">
+                <div className="w-full lg:p-10 text-white">
                     <Outlet></Outlet>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,34 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from './Layout/Main';
+import Home from './pages/Home/Home';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Profile from './pages/Profile/Profile';
+import Help from './pages/Help/Help';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard></Dashboard>
+      },
+      {
+        path: '/profile',
+        element: <Profile></Profile>
+      },
+      {
+        path: '/help',
+        element: <Help></Help>
+      }
+    ]
   },
 ]);
 
