41eb50d7046e115e818d06cbad75d5799e3d9a76
Li Pon <mirnatul@gmail.com>
2023-10-06 01:23:33 +0600

    tailwind and daisyui set up

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <button className="btn">Button</button>
+            <button className="btn btn-neutral">Neutral</button>
+            <button className="btn btn-primary">Primary</button>
+            <button className="btn btn-secondary">Secondary</button>
+            <button className="btn btn-accent">Accent</button>
+            <button className="btn btn-ghost">Ghost</button>
+            <button className="btn btn-link">Link</button>
+        </div>
+    );
+};
+
+export default Main;
